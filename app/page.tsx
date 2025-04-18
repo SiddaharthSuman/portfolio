3f630820c045ad5ef726f34fd2dbd9930406d52e
Siddaharth Suman <siddaharthsuman@gmail.com>
2025-04-18 04:28:10 -0400

    config: add base styles

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ export default function Home() {
   return (
     // <Container >
     <div>
-      <h1 style={{ fontFamily: 'var(--font-geist-sans)' }}>Hello From Next.js 13.4</h1>
-      <p style={{ fontFamily: 'var(--font-manrope)' }}>
+      <h1>Hello From Next.js 13.4</h1>
+      <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi assumenda facere amet sed
         sit! Eligendi itaque harum nobis? Reprehenderit id perspiciatis et molestias animi
         asperiores numquam ratione nam expedita repellendus?
